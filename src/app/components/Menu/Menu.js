d9625d92720fbe245917c9eb50cf0f6712f37e3d
Holyolg <oleg.chikolovec@gmail.com>
2024-04-14 23:19:42 +0300

    new project initial

--- src/app/components/Menu/Menu.js
+++ src/app/components/Menu/Menu.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Menu() {
-	return <div className="menu"></div>;
+	return (
+		<div className="menu">
+			<div className="menu-logo">
+				<Link href="/">TIK</Link>
+			</div>
+			<div className="menu-logo">
+				<Link href="/project">Project</Link>
+			</div>
+			<div className="menu-logo">
+				<Link href="/contacts">TIK</Link>
+			</div>
+		</div>
+	);
 }
