52a85d13c256f7395062cb3e1711cf6ba8aa39e1
Holy <oleg.chikolovec@gmail.com>
2024-05-08 16:08:50 +0300

    fix overflow

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import VerticalAccordion from "./components/Accordion/Accordion";
 
 export default function Home() {
 	return (
-		<main className="bg-[#003056]">
-			<div className="hero-wrapper">
+		<main className="">
+			<div className="hero-wrapper overflow-hidden">
 				<VerticalAccordion />
 			</div>
 		</main>
