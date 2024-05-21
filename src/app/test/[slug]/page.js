fb8b64131f6c6e668b83513701284ccc2add49ac
Holy <oleg.chikolovec@gmail.com>
2024-05-21 11:49:46 +0300

    del test

--- src/app/test/[slug]/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-export default function CardCategory({ params }) {
-	params = "concept";
-	useRouter().push("/concept?category=2");
-}
--- src/app/test/page.js
+++
@@ -1,5 +0,0 @@
-import { Cards } from "@/app/components/Cards/Cards";
-
-export default async function Concept() {
-	return <Cards category={"concept"} />;
-}
