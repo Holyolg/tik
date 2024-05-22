f35b37870ab1c829b2cafecf7d1394796fb8dbf2
Holyolg <oleg.chikolovec@gmail.com>
2024-05-22 17:16:29 +0300

    add Map

--- src/app/components/Map/Map.js
+++ src/app/components/Map/Map.js
@@ -1,34 +1,44 @@
-'use client';
-import Image from 'next/image';
-import React from 'react';
-import ReactDOM from 'react-dom';
+"use client";
+import Image from "next/image";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const ymaps3Reactify = await ymaps3.import('@yandex/ymaps3-reactify');
+const ymaps3Reactify = await ymaps3.import("@yandex/ymaps3-reactify");
 const reactify = ymaps3Reactify.reactify.bindTo(React, ReactDOM);
-const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapControls, YMapMarker } =
-  reactify.module(ymaps3);
+const {
+	YMap,
+	YMapDefaultSchemeLayer,
+	YMapDefaultFeaturesLayer,
+	YMapControls,
+	YMapMarker,
+} = reactify.module(ymaps3);
 
-const { YMapZoomControl } = reactify.module(await ymaps3.import('@yandex/ymaps3-controls@0.0.1'));
+const { YMapZoomControl } = reactify.module(
+	await ymaps3.import("@yandex/ymaps3-controls@0.0.1")
+);
 
 const Map = () => {
-  const location = { center: [55.751574, 37.573856], zoom: 13 };
+	const location = { center: [55.751574, 37.573856], zoom: 13 };
 
-  return (
-    <YMap location={location} className="min-h-[calc(100vh-300px)] md:min-h-[calc(100vh-280px)]">
-      <YMapControls position="left">
-        <YMapZoomControl />
-      </YMapControls>
+	return (
+		<YMap
+			location={location}
+			className="min-h-[calc(100vh-300px)] md:min-h-[calc(100vh-280px)]"
+		>
+			<YMapControls position="left">
+				<YMapZoomControl />
+			</YMapControls>
 
-      <YMapDefaultSchemeLayer />
-      <YMapDefaultFeaturesLayer />
+			<YMapDefaultSchemeLayer />
+			<YMapDefaultFeaturesLayer />
 
-      <YMapMarker coordinates={location.center} zIndex={1}>
-        <div className="relative h-[56px] w-[46px]">
-          <Image src="/icon-location.svg" alt="location" fill />
-        </div>
-      </YMapMarker>
-    </YMap>
-  );
+			<YMapMarker coordinates={location.center} zIndex={1}>
+				<div className="relative h-[56px] w-[46px]">
+					<Image src="/icon-location.svg" alt="location" fill />
+				</div>
+			</YMapMarker>
+		</YMap>
+	);
 };
 
 export default Map;
--- src/app/components/YMapLoader/YMapLoader.js
+++ src/app/components/YMapLoader/YMapLoader.js
@@ -1,15 +1,15 @@
-import Script from 'next/script';
+import Script from "next/script";
 
-const MAP_API_KEY = process.env.MAP_API_KEY;
+// const MAP_API_KEY = process.env.MAP_API_KEY;
 
 export const YMapLoader = () => {
-  return (
-    <>
-      <Script
-        src={`https://api-maps.yandex.ru/3.0/?apikey=12329852-e278-414d-a397-97eb8883b57f&lang=en_US`}
-        type="module"
-        strategy="beforeInteractive"
-      />
-    </>
-  );
+	return (
+		<>
+			<Script
+				src={`https://api-maps.yandex.ru/3.0/?apikey=12329852-e278-414d-a397-97eb8883b57f&lang=en_US`}
+				type="module"
+				strategy="beforeInteractive"
+			/>
+		</>
+	);
 };
