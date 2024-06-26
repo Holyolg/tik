1a21b101ef412bfe0532aca1d21b04798b3f414f
Holyolg <oleg.chikolovec@gmail.com>
2024-06-27 00:49:56 +0300

    del test file

--- src/app/test/concept/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Cards } from "../../components/Cards/Cards";
-
-export default async function Concept() {
-	return (
-		<section className="min-h-[100vh]">
-			<Cards category={"concept"} />
-		</section>
-	);
-}
--- src/app/test/genproject/conceptual/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-export default function Redirect() {
-	useRouter().push("/genproject?category=2");
-}
