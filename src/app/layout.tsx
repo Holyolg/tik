f96fd14fcfbf4535b48f9a42899f4e90b2e249d4
Holyolg <oleg.chikolovec@gmail.com>
2024-07-06 14:11:24 +0300

    /

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import { Suspense } from "react";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
@@ -24,29 +23,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="ru">
-			<Head>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-				<meta name="msapplication-TileColor" content="#da532c" />
-				<meta name="theme-color" content="#ffffff" />
-			</Head>
 			<body
 				className={`${inter.className} min-h-[100vh] flex flex-col justify-between`}
 			>
