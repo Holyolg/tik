261319932132f29458182e10dc81e79002b5d2f9
Holy <oleg.chikolovec@gmail.com>
2024-07-19 16:28:58 +0300

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,19 @@ import { useRef } from "react";
 import { AnimateAbout } from "./components/PageComponents/AnimateAbout/AnimateAbout";
 import { Hero } from "./components/PageComponents/Hero/Hero";
 import HeroProjects from "./components/PageComponents/HeroProjects/HeroProjects";
-import Loading from "./loading";
 
 export default function Home() {
-	const scrollRef = useRef<HTMLElement | any>();
+  const scrollRef = useRef<HTMLElement | any>();
 
-	return (
-		<main>
-			<Hero scroll={scrollRef} />
-			<section className="container mx-auto mt-14 md:mt-32" ref={scrollRef}>
-				<AnimateAbout />
-			</section>
-			<section className="mx-auto container mt-14 md:mt-32">
-				<HeroProjects numItems={9} />
-			</section>
-			{/* <section className="mx-auto container mt-14 md:mt-32" id="contacts">
-				<Contacts />
-			</section> */}
-		</main>
-	);
+  return (
+    <main>
+      <Hero scroll={scrollRef} />
+      <section className="container mx-auto mt-14 md:mt-32" ref={scrollRef}>
+        <AnimateAbout />
+      </section>
+      <section className="mx-auto container mt-14 md:mt-32">
+        <HeroProjects numItems={9} />
+      </section>
+    </main>
+  );
 }
--- src/app/projects/[type]/[id]/page.tsx
+++ src/app/projects/[type]/[id]/page.tsx
@@ -1,14 +1,31 @@
 import CardDetails from "@/app/components/CardDetails/CardDetails";
-import { notFound } from "next/navigation";
 import getCards from "@/app/services/GetCards/GetCards";
+import { Metadata, ResolvingMetadata } from "next";
 
+type Props = {
+  params: { id: string; type: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { id, type } = params;
+
+  const card = await fetch(
+    `https://668e955fbf9912d4c92ee8b3.mockapi.io/${type}/${id}`
+  ).then((res) => res.json());
+  return {
+    title: card.title,
+  };
+}
 export default async function CardDetailsPage({
   params,
 }: {
   params: { id: string; type: string };
 }) {
-  const id = params.id;
-  const type = params.type;
+  const { id, type } = params;
   const API_URL = `https://668e955fbf9912d4c92ee8b3.mockapi.io/${type}/${id}`;
   const card = await getCards(API_URL);
 
