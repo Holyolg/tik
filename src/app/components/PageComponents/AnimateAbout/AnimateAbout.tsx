1cecbd64efe689208aae1290fe6cc54e02cf53fe
Holy <oleg.chikolovec@gmail.com>
2024-07-23 16:59:38 +0300

    update

--- src/app/components/PageComponents/AnimateAbout/AnimateAbout.tsx
+++ src/app/components/PageComponents/AnimateAbout/AnimateAbout.tsx
@@ -10,63 +10,63 @@ import { Arrow } from "@/app/ui/Arrow/Arrow";
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 export const AnimateAbout = () => {
-	const animateRef = useRef<HTMLElement | any>();
+  const animateRef = useRef<HTMLElement | any>();
 
-	//анимация
-	useGSAP(
-		() => {
-			const texts = gsap.utils.toArray(".animate_text");
-			texts.forEach((text: any) => {
-				gsap.from(text, {
-					opacity: 0,
-					x: -200,
-					scrollTrigger: {
-						trigger: text,
-						start: "bottom 90%",
-						end: "top 40%",
+  //анимация
+  useGSAP(
+    () => {
+      const texts = gsap.utils.toArray(".animate_text");
+      texts.forEach((text: any) => {
+        gsap.from(text, {
+          opacity: 0,
+          x: -200,
+          scrollTrigger: {
+            trigger: text,
+            start: "bottom 90%",
+            end: "top 40%",
 
-						scrub: true,
-						//markers: true,
-					},
-				});
-			});
-		},
-		{ scope: animateRef }
-	);
+            scrub: true,
+            //markers: true,
+          },
+        });
+      });
+    },
+    { scope: animateRef }
+  );
 
-	return (
-		<>
-			<div className="flex justify-between items-center">
-				<h2 className="text-4xl lg:text-6xl font-semibold">Мы</h2>
-				<button className="hover:opacity-70 flex items-center cursor-pointer space-x-2">
-					<Link href={"/about"}>Подробнее о нас</Link>
-					<Arrow/>
-				</button>
-			</div>
-			<div className="flex flex-col lg:flex-row justify-between w-full">
-				<div className="lg:w-2/5">
-					<h2 className="text-4xl lg:text-6xl" ref={animateRef}>
-						<div className="animate_text">инженеры</div>
-						<div className="animate_text">архитекторы</div>
-						<div className="animate_text">конструкторы</div>
-						<div className="animate_text">BIM-специалисты</div>
-					</h2>
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <h2 className="text-4xl lg:text-6xl font-semibold">Мы</h2>
+        <button className="hover:opacity-70 flex items-center cursor-pointer space-x-2">
+          <Link href={"/about"}>Подробнее о нас</Link>
+          <Arrow />
+        </button>
+      </div>
+      <div className="flex flex-col lg:flex-row justify-between w-full">
+        <div className="lg:w-2/5">
+          <h2 className="text-4xl lg:text-6xl" ref={animateRef}>
+            <div className="animate_text">инженеры</div>
+            <div className="animate_text">архитекторы</div>
+            <div className="animate_text">конструкторы</div>
+            <div className="animate_text">BIM-специалисты</div>
+          </h2>
 
-					<div className="rounded-lg mt-6 text-gray-500 leading-relaxed">
-						Проектируем жилые и общественные здания, образовательные учреждения,
-						офисы и деловые центры, индивидуальные жилые здания и интерьеры, а
-						также занимаемся разработкой комфортной городской среды.
-					</div>
-				</div>
-				<div className="relative mt-6 lg:mt-0">
-					<Image
-						className="rounded-lg grayscale"
-						src={about}
-						width={650}
-						alt="наше фото"
-					></Image>
-				</div>
-			</div>
-		</>
-	);
+          <div className="rounded-lg mt-6 text-gray-500 leading-relaxed">
+            Проектируем жилые и многофункциональные комплексы, офисные здания и
+            торговые центры, социальные объекты, а также занимаемся разработкой
+            архитектурных концепций, интерьеров и комфортной городской среды.
+          </div>
+        </div>
+        <div className="relative mt-6 lg:mt-0">
+          <Image
+            className="rounded-lg grayscale"
+            src={about}
+            width={650}
+            alt="наше фото"
+          ></Image>
+        </div>
+      </div>
+    </>
+  );
 };
