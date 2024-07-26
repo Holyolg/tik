eecade653a904ac2aeae242d14806fa481c3f516
Holy <oleg.chikolovec@gmail.com>
2024-07-26 15:15:30 +0300

    test

--- src/app/components/Video/Video.tsx
+++ src/app/components/Video/Video.tsx
@@ -2,23 +2,24 @@
 import { useState } from "react";
 
 export const Video = ({ src, ...props }: { src: string }) => {
-	const [isWaiting, setIsWaiting] = useState(false);
-	return (
-		<video
-			{...props}
-			src={src}
-			className={
-				isWaiting
-					? "blur-lg md:w-full md:h-[100vh] h-[100vh] object-cover brightness-50"
-					: "md:w-full md:h-[100vh] h-[100vh] object-cover brightness-50"
-			}
-			width="100%"
-			height="100%"
-			preload="none"
-			autoPlay
-			loop
-			muted
-			playsInline
-		/>
-	);
+  const [isWaiting, setIsWaiting] = useState(false);
+  return (
+    <video
+      {...props}
+      className={
+        isWaiting
+          ? "blur-lg md:w-full md:h-[100vh] h-[100vh] object-cover brightness-50"
+          : "md:w-full md:h-[100vh] h-[100vh] object-cover brightness-50"
+      }
+      width="100%"
+      height="100%"
+      preload="none"
+      autoPlay
+      loop
+      muted
+      playsInline
+    >
+      <source src={src} type="video/mp4" />
+    </video>
+  );
 };
