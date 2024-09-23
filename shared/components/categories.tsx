73a96d5d3ef0db1599247880714e295b145c1f4e
Holyolg <oleg.chikolovec@gmail.com>
2024-09-23 20:07:48 +0300

    fix

--- shared/components/categories.tsx
+++ shared/components/categories.tsx
@@ -3,67 +3,68 @@ import { cardStore } from "@/shared/store/store";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 
+export type category =
+  | "Все"
+  | "Здания"
+  | "Жилые"
+  | "Общественные"
+  | "Интерьеры"
+  | "Благоустройство";
 type Props = {
-	type: string;
-	category: string;
+  type: string;
+  category: category;
 };
 
 export const Categories: React.FC<Props> = ({ type, category }) => {
-	const categoriesProject: string[] = [
-		"Все",
-		"Жилые",
-		"Общественные",
-		"Интерьеры",
-		"Благоустройство",
-	];
+  const categoriesProject: category[] = [
+    "Все",
+    "Жилые",
+    "Общественные",
+    "Интерьеры",
+    "Благоустройство",
+  ];
 
-	const categoriesConcept: string[] = [
-		"Все",
-		"Здания",
-		"Интерьеры",
-		"Благоустройство",
-	];
+  const categoriesConcept: category[] = ["Все", "Здания", "Интерьеры", "Благоустройство"];
 
-	const updateCategory = cardStore((state) => state.updateCategory);
+  const updateCategory = cardStore(state => state.updateCategory);
 
-	const categoryValue =
-		type == "genproject" ? categoriesProject : categoriesConcept;
+  const categoryValue = type == "genproject" ? categoriesProject : categoriesConcept;
 
-	const router = useRouter();
-	const pathname = usePathname();
-	const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-	const createQueryString = useCallback(
-		(name: string, value: string) => {
-			const params = new URLSearchParams(searchParams.toString());
-			params.set(name, value);
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
 
-			return params.toString();
-		},
-		[searchParams]
-	);
+      return params.toString();
+    },
+    [searchParams]
+  );
 
-	return (
-		<nav className="flex space-x-10 items-center overflow-x-auto snap-x snap-mandatory mt-6 xl:mt-0">
-			{categoryValue.map((categoryName) => (
-				<span
-					onClick={() => {
-						updateCategory(categoryName);
-						router.push(
-							`${pathname}?
+  return (
+    <nav className="flex space-x-10 items-center overflow-x-auto snap-x snap-mandatory mt-6 xl:mt-0">
+      {categoryValue.map(categoryName => (
+        <span
+          onClick={() => {
+            updateCategory(categoryName);
+            router.push(
+              `${pathname}?
 								${createQueryString("category", categoryName)}`,
-							{
-								scroll: false,
-							}
-						);
-					}}
-					key={categoryName}
-					className={`snap-always snap-center hover:opacity-50 cursor-pointer ${
-						category == categoryName ? "font-semibold" : " "
-					}`}>
-					{categoryName}
-				</span>
-			))}
-		</nav>
-	);
+              {
+                scroll: false,
+              }
+            );
+          }}
+          key={categoryName}
+          className={`snap-always snap-center hover:opacity-50 cursor-pointer ${
+            category == categoryName ? "font-semibold" : " "
+          }`}>
+          {categoryName}
+        </span>
+      ))}
+    </nav>
+  );
 };
--- shared/store/store.tsx
+++ shared/store/store.tsx
@@ -1,53 +1,32 @@
 import { create } from "zustand";
+import { category } from "../components/categories";
 
-type State = {
-	loading: boolean;
-	type: "genproject" | "concept";
-	category:
-		| "Все"
-		| "Здания"
-		| "Жилые"
-		| "Общественные"
-		| "Интерьеры"
-		| "Благоустроство";
+export type State = {
+  loading: boolean;
+  type: "genproject" | "concept";
+  category: category;
 };
 
 type Action = {
-	updateLoading: (loading: boolean) => void;
-	updateType: (type: "genproject" | "concept") => void;
-	updateCategory: (
-		category:
-			| "Все"
-			| "Здания"
-			| "Жилые"
-			| "Общественные"
-			| "Интерьеры"
-			| "Благоустроство"
-	) => void;
+  updateLoading: (loading: boolean) => void;
+  updateType: (type: "genproject" | "concept") => void;
+  updateCategory: (category: category) => void;
 };
 
-export const cardStore = create<State & Action>((set) => ({
-	loading: false,
-	updateLoading: (loading: boolean) =>
-		set(() => ({
-			loading: loading,
-		})),
-	type: "genproject",
-	updateType: (type: "genproject" | "concept") =>
-		set(() => ({
-			type: type,
-		})),
-	category: "Все",
-	updateCategory: (
-		category:
-			| "Все"
-			| "Здания"
-			| "Жилые"
-			| "Общественные"
-			| "Интерьеры"
-			| "Благоустроство"
-	) =>
-		set(() => ({
-			category: category,
-		})),
+export const cardStore = create<State & Action>(set => ({
+  loading: false,
+  updateLoading: (loading: boolean) =>
+    set(() => ({
+      loading: loading,
+    })),
+  type: "genproject",
+  updateType: (type: "genproject" | "concept") =>
+    set(() => ({
+      type: type,
+    })),
+  category: "Все",
+  updateCategory: (category: category) =>
+    set(() => ({
+      category: category,
+    })),
 }));
